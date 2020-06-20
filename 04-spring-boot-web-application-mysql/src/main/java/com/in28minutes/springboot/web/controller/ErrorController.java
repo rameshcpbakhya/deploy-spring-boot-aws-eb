ce288585cadf1bd51b5596be3ae1846b6bbc9d23
rameshcpbakhya <rameshcpbakhya@gmail.com>
2020-06-20 14:58:26 +0530

    Update ErrorController.java

--- 04-spring-boot-web-application-mysql/src/main/java/com/in28minutes/springboot/web/controller/ErrorController.java
+++ 04-spring-boot-web-application-mysql/src/main/java/com/in28minutes/springboot/web/controller/ErrorController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+//Controller
 @Controller("error")
 public class ErrorController {
 	
